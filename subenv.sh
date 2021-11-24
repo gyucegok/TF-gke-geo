e6cf4e0f6efe96dcb678b22bc230ab7b56b5c20d
Gokhan Yucegok <gyucegok@gmail.com>
2021-11-24 08:55:12 +0000

    v0.14

--- subenv.sh
+++ subenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for template in $(find . -name '*.tf-tmplate'); do envsubst < ${template} > ${template%.*.tf}; done
+for template in $(find . -name '*.tmplate'); do envsubst < ${template} > ${template%.*}; done
